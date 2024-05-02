0eddb1585780ff9dd6791b06511b4510790dc25d
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-05-02 11:10:57 +0530

    Update prac9.java (no user input)

--- py/prac9.java
+++ py/prac9.java
@@ -1,5 +1,48 @@
 import java.util.*;
 
+public class Main {
+    static void heapify(int arr[], int n, int ind) {
+        int largest = ind;
+        int n1 = 2 * ind + 1;
+        int n2 = 2 * ind + 2;
+        if (n1 < n && arr[largest] < arr[n1]) {
+            largest = n1;
+        }
+        if (n2 < n && arr[largest] < arr[n2]) {
+            largest = n2;
+        }
+        if (ind != largest) {
+            int temp = arr[ind];
+            arr[ind] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // Fixed size of the array
+        int arr[] = {4, 10, 3, 5, 1, 8, 2, 7, 6, 9}; // Array initialization
+        
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0); // Adjust the heap size to i instead of n
+        }
+        System.out.println("Final Sorted Array: ");
+        for (int j = 0; j < n; j++) {
+            System.out.print(arr[j] + " ");
+        }
+        System.out.println(" ");
+    }
+}
+
+
+/*import java.util.*;
+
 class Main {
     static void heapify(int arr[], int n, int ind) {
         int largest = ind;
@@ -52,3 +95,4 @@ class Main {
         System.out.println(" ");
     }
 }
+*/
